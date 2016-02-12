989f26b56197f7853190dec5e1856a01d0f3307f
xaqq <kapp.arno@gmail.com>
2016-02-12 14:31:00 +0100

    Start trying to integrate sb-admin theme

--- web/ember-cli-build.js
+++ web/ember-cli-build.js
@@ -19,6 +19,29 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+
+  app.import('bower_components/font-awesome/fonts/fontawesome-webfont.ttf',
+    {destDir: 'fonts'});
+  app.import('bower_components/font-awesome/fonts/fontawesome-webfont.woff',
+    {destDir: 'fonts'});
+
   app.import('app/styles/form-signin.css');
+
+  app.import('bower_components/bootstrap/dist/css/bootstrap.min.css');
+  app.import('bower_components/metisMenu/dist/metisMenu.min.css');
+
+  app.import('bower_components/startbootstrap-sb-admin-2/dist/css/sb-admin-2.css');
+  app.import('bower_components/startbootstrap-sb-admin-2/dist/css/timeline.css');
+
+  app.import('bower_components/morrisjs/morris.css');
+  app.import('bower_components/font-awesome/css/font-awesome.min.css');
+
+  //app.import('bower_components/jquery/dist/jquery.min.js');
+  app.import('bower_components/metisMenu/dist/metisMenu.min.js');
+  app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');
+  app.import('bower_components/raphael/raphael-min.js');
+  app.import('bower_components/morrisjs/morris.min.js');
+
+  app.import('bower_components/startbootstrap-sb-admin-2/dist/js/sb-admin-2.js');
   return app.toTree();
 };
